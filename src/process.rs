db262e1061bf0a5be012273079a96345453612a1
dalance <dalance@gmail.com>
2019-02-18 11:19:52 +0900

    Fix

--- src/process.rs
+++ src/process.rs
@@ -61,7 +61,6 @@ pub struct ProcessInfo {
 
 #[cfg(target_os = "macos")]
 pub fn collect_proc(interval: Duration) -> Vec<ProcessInfo> {
-    let mut base_procs = Vec::new();
     let mut ret = Vec::new();
 
     let mut ctl = sysctl::Ctl::new("kern.proc.all").unwrap();
